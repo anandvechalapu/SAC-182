a3385c257d4393cdf64c72e13c7b98c57795e54d
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-14 17:52:35 +0530

    Updated

--- repository/JiraActivationRepository.java
+++ repository/JiraActivationRepository.java
@@ -1,44 +1,13 @@
-@Repository
-public class JiraActivationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
-  private String username;
-  private String password;
-  private String jiraUrl;
+public interface JiraActivationRepository extends JpaRepository<JiraActivation, Long> {
 
-  public String getUsername() {
-    return this.username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return this.password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getJiraUrl() {
-    return this.jiraUrl;
-  }
-
-  public void setJiraUrl(String jiraUrl) {
-    this.jiraUrl = jiraUrl;
-  }
-
-  public boolean validateCredentials() {
-    // Validate the provided Jira credentials through a JAVA API that hits the Jira API.
-  }
-
-  public boolean authenticateUser() {
-    // Use the Jira provided endpoint to authenticate the user and return the response.
-  }
-
-  public String getErrorMessage() {
-    // If the Jira credentials are invalid, the system must display an error message.
-  }
-
+	public JiraActivation findByUsernameAndPasswordAndUrl(String username, String password, String url);
+	
+	public boolean validateJiraCredentials(String username, String password, String url);
+	
+	public String authenticateUser(String username, String password, String url);
+	
+	public String displayErrorMessage(String username, String password, String url);
+	
 }
